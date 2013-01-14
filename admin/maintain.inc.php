653f6a7c3f3a9129e38ab2a555254fa6fed5d71d
flop25 <flop25@piwigo.org>
2013-01-14 19:14:00 +0000

    derivative size
    git-svn-id: http://piwigo.org/svn/extensions/hr_os@20154 68402e56-0260-453c-a942-63ccdbb3a9ee

--- admin/maintain.inc.php
+++ admin/maintain.inc.php
@@ -19,6 +19,17 @@ VALUES ("hr_os" , "'.pwg_db_real_escape_string(serialize($config)).'" , "hr_os p
 
     pwg_query($query);
   }
+  if (isset($conf['derivatives']))  {    
+    $new = @unserialize($conf['derivatives']);
+    $new['d']['Optimal']=ImageStdParams::get_custom(730,9999); 
+    $query = '
+        UPDATE '.CONFIG_TABLE.'
+        SET value="'.pwg_db_real_escape_string(serialize($new)).'"
+        WHERE param = "derivatives"
+        LIMIT 1';
+    pwg_query($query);
+    load_conf_from_db();
+  }
 }
 
 function theme_deactivate()
@@ -27,6 +38,20 @@ function theme_deactivate()
 
   $query = 'DELETE FROM ' . CONFIG_TABLE . ' WHERE param="hr_os" LIMIT 1;';
   pwg_query($query);
+  if (isset($conf['derivatives']))  {    
+    $new = @unserialize($conf['derivatives']);
+    if( isset($new['d']['Optimal']))
+    {
+      unset($new['d']['Optimal']);
+      $conf['derivatives']=serialize($new);
+      $query = '
+          UPDATE '.CONFIG_TABLE.'
+          SET value="'.addslashes(serialize($new)).'"
+          WHERE param = "derivatives"
+          LIMIT 1';
+      pwg_query($query);
+    }
+  }
 }
 
 ?>
